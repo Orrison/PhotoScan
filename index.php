a647bed55a89c3d0c4b6353c0a799c695d94f3a1
Kevin Ullyott <ullyott.kevin@gmail.com>
2018-07-02 23:24:20 -0700

    Fix CSS directory path for photoscan site

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ if ($_FILES) {
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="/_css/styles.css">
+    <link rel="stylesheet" type="text/css" href="_css/styles.css">
 
     <title>PhotoScan | Kevin Ullyott</title>
     
