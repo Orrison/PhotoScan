cb48b857edae6046c6223bc6aa422a2971d5ceaa
Kevin Ullyott <ullyott.kevin@gmail.com>
2018-07-02 22:40:29 -0700

    PSR Refactoring and name change

--- index.php
+++ index.php
@@ -7,22 +7,22 @@
  * @param image $file
  * @return multitype:number |boolean
  */
-function read_gps_location($file){
+function read_gps_location($file)
+{
     if (is_file($file)) {
         $info = exif_read_data($file);
         if (isset($info['GPSLatitude']) && isset($info['GPSLongitude']) &&
             isset($info['GPSLatitudeRef']) && isset($info['GPSLongitudeRef']) &&
             in_array($info['GPSLatitudeRef'], array('E','W','N','S')) && in_array($info['GPSLongitudeRef'], array('E','W','N','S'))) {
-
             $GPSLatitudeRef  = strtolower(trim($info['GPSLatitudeRef']));
             $GPSLongitudeRef = strtolower(trim($info['GPSLongitudeRef']));
 
-            $lat_degrees_a = explode('/',$info['GPSLatitude'][0]);
-            $lat_minutes_a = explode('/',$info['GPSLatitude'][1]);
-            $lat_seconds_a = explode('/',$info['GPSLatitude'][2]);
-            $lng_degrees_a = explode('/',$info['GPSLongitude'][0]);
-            $lng_minutes_a = explode('/',$info['GPSLongitude'][1]);
-            $lng_seconds_a = explode('/',$info['GPSLongitude'][2]);
+            $lat_degrees_a = explode('/', $info['GPSLatitude'][0]);
+            $lat_minutes_a = explode('/', $info['GPSLatitude'][1]);
+            $lat_seconds_a = explode('/', $info['GPSLatitude'][2]);
+            $lng_degrees_a = explode('/', $info['GPSLongitude'][0]);
+            $lng_minutes_a = explode('/', $info['GPSLongitude'][1]);
+            $lng_seconds_a = explode('/', $info['GPSLongitude'][2]);
 
             $lat_degrees = $lat_degrees_a[0] / $lat_degrees_a[1];
             $lat_minutes = $lat_minutes_a[0] / $lat_minutes_a[1];
@@ -34,7 +34,7 @@ function read_gps_location($file){
             $lat = (float) $lat_degrees+((($lat_minutes*60)+($lat_seconds))/3600);
             $lng = (float) $lng_degrees+((($lng_minutes*60)+($lng_seconds))/3600);
 
-            //If the latitude is South, make it negative. 
+            //If the latitude is South, make it negative.
             //If the longitude is west, make it negative
             $GPSLatitudeRef  == 's' ? $lat *= -1 : '';
             $GPSLongitudeRef == 'w' ? $lng *= -1 : '';
@@ -43,48 +43,49 @@ function read_gps_location($file){
                 'lat' => $lat,
                 'lng' => $lng
             );
-        }           
+        }
     }
     return false;
 }
 
-function convertToDecimal($fraction) {
-  $numbers=explode("/",$fraction);
-  return round($numbers[0]/$numbers[1],6);
+function convertToDecimal($fraction)
+{
+    $numbers=explode("/", $fraction);
+    return round($numbers[0]/$numbers[1], 6);
 }
 
 if ($_FILES) {
-  $file = $_FILES["photoUpload"]["tmp_name"];
+    $file = $_FILES["photoUpload"]["tmp_name"];
 
-  $exifData = exif_read_data($file);
+    $exifData = exif_read_data($file);
 
-  $dateTime = date_format($exifData['DateTimeOriginal'], 'n/j/Y g:i:s A');
+    $dateTime = date_format($exifData['DateTimeOriginal'], 'n/j/Y g:i:s A');
 
-  $location = read_gps_location($file);
+    $location = read_gps_location($file);
 
-  $degrees = convertToDecimal($exifData['GPSImgDirection']);
+    $degrees = convertToDecimal($exifData['GPSImgDirection']);
 
-  $direction = '';
+    $direction = '';
 
-  if ($degrees == 0) {
-    $direction = 'North';
-  } elseif ($degrees > 0 && $degrees < 90) {
-    $direction = 'North East';
-  } elseif ($degrees == 90) {
-    $direction = 'East';
-  } elseif ($degrees > 90 && $degrees < 180) {
-    $direction = 'South East';
-  } elseif ($degrees == 180) {
-    $direction = 'South';
-  } elseif ($degrees > 180 && $degrees < 270) {
-    $direction = 'South West';
-  } elseif ($degrees == 270) {
-    $direction = 'West';
-  } elseif ($degrees > 270 && $degrees < 360) {
-    $direction = 'North West';
-  }
+    if ($degrees == 0) {
+        $direction = 'North';
+    } elseif ($degrees > 0 && $degrees < 90) {
+        $direction = 'North East';
+    } elseif ($degrees == 90) {
+        $direction = 'East';
+    } elseif ($degrees > 90 && $degrees < 180) {
+        $direction = 'South East';
+    } elseif ($degrees == 180) {
+        $direction = 'South';
+    } elseif ($degrees > 180 && $degrees < 270) {
+        $direction = 'South West';
+    } elseif ($degrees == 270) {
+        $direction = 'West';
+    } elseif ($degrees > 270 && $degrees < 360) {
+        $direction = 'North West';
+    }
 
-  $cleanData = array('Make' => $exifData['Make'], 'Model' => $exifData['Model'], 'DateTime' => $exifData['DateTimeOriginal'], 'lat' => $location['lat'], 'lng' => $location['lng'], 'degrees' => $degrees, 'direction' => $direction);
+    $cleanData = array('Make' => $exifData['Make'], 'Model' => $exifData['Model'], 'DateTime' => $exifData['DateTimeOriginal'], 'lat' => $location['lat'], 'lng' => $location['lng'], 'degrees' => $degrees, 'direction' => $direction);
 }
 
 ?>
@@ -99,7 +100,7 @@ if ($_FILES) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/_css/styles.css">
 
-    <title>PhotoStalk | Kevin Ullyott</title>
+    <title>PhotoScan | Kevin Ullyott</title>
     
   </head>
   <body>
@@ -115,7 +116,8 @@ if ($_FILES) {
             </div>
           </div>
           <nav class="navbar navbar-dark bg-dark">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="     #navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false"     aria-label="Toggle navigation">
+            <a class="navbar-brand" href="#">PhotoScan by KEVIN ULLYOTT</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
           </nav>
@@ -145,6 +147,7 @@ if ($_FILES) {
 
           <form action="" method="post" enctype="multipart/form-data">
             <input type="file" name="photoUpload"/>
+            <input type="submit" name="submit" value="Scan"/>
           </form>
 
         <?php endif; ?>
