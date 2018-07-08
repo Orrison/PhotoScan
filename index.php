1a2d55bfad6bcc961bb83722dda634cf47246a03
Kevin Ullyott <ullyott.kevin@gmail.com>
2018-07-08 12:48:41 -0700

    Update hidden content text and add link to main portfolio site

--- index.php
+++ index.php
@@ -112,12 +112,11 @@ if ($_FILES) {
           <div class="collapse" id="navbarToggleExternalContent">
             <div class="bg-dark p-4">
               <h4 class="text-white">What is this?</h4>
-              <span class="text-muted">PhotoScan is a lightweight application developed to scan uploaded images for location specific EXIF data. The photos we take every day contain more info then just our smiling faces and what food we ate that day. They can contain information as to where the photo was taken, at what time, and even the diretion the photo was taken. Many sites strip thise data once the photo is uploaded. Though some keep the information for their own use and even worse some do not strip the data at all. Make sure the sites you upload your photos too are trust worthy and well protected!
-              </span>
+              <span class="text-muted">PhotoScan is a lightweight application developed to scan uploaded images for location specific EXIF data. The photos we take every day contain more info then just our smiling faces and what food we ate that day. They can contain information as to where the photo was taken, at what time, and even the direction the photo was taken. Many sites strip this data once the photo is uploaded. Though some keep the information for their own use and even worse some do not strip the data at all. Make sure the sites you upload your photos too are trust worthy and well protected!</span>
             </div>
           </div>
           <nav class="navbar navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">PhotoScan by KEVIN ULLYOTT</a>
+            <span class="navbar-brand">PhotoScan by <a href="https://www.kevinullyott.com">KEVIN ULLYOTT</a></span>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
