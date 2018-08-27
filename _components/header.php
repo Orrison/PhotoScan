a1ce26d51ad1b2941219c784ede0ed3ee27b5eb5
Kevin Ullyott <ullyott.kevin@gmail.com>
2018-08-26 22:06:14 -0700

    Update YML and test

--- _components/header.php
+++ _components/header.php
@@ -1,7 +1,7 @@
 <div class="col-md-12">
     <div class="collapse" id="navbarToggleExternalContent">
         <div class="bg-dark p-4">
-            <h4 class="text-white">What is this?</h4>
+            <h4 class="text-white">What is this? Test</h4>
             <span class="text-muted">PhotoScan is a lightweight application developed to scan uploaded images for location specific EXIF data. The photos we take every day contain more info then just our smiling faces and what food we ate that day. They can contain information as to where the photo was taken, at what time, and even the direction the photo was taken. Many sites strip this data once the photo is uploaded. Though some keep the information for their own use and even worse some do not strip the data at all. Make sure the sites you upload your photos too are trust worthy and well protected!</span>
         </div>
     </div>
