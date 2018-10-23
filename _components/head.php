4c65ea8e8d6ba17e0614a75a0ddecc33ecbebb42
Alec <alec.espiritu@gmail.com>
2018-10-23 15:46:04 -0700

    Removed Double Space

--- _components/head.php
+++ _components/head.php
@@ -7,7 +7,6 @@
 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
   
-
 <link rel="stylesheet" type="text/css" href="_css/styles.css">
 
 <title>PhotoScan | Kevin Ullyott</title>
